6fd9b133d4a14623a82e73f4bb2f04799218e1d1
srilu27533 <98971060+srilu27533@users.noreply.github.com>
2022-02-03 21:51:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from main import text_to_speech
+import os
+    
+app = Flask(__name__)
+@app.route("/")
+def FUN_root():
+    return render_template("frontend.html")
+
+
+@app.route('/regact', methods=['POST', 'GET'])
+def homepage():
+    if request.method == 'POST':
+        text = request.form['speech']
+        gender = request.form['voices']
+        text_to_speech(text, gender)
+        return render_template('frontend.html')
+    else:
+        return render_template('frontend.html')
+
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
